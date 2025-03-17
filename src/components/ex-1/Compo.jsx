323e087e0c3bad1517a3dfb230db1cf925b3703b
johannpa <johann_paque@hotmail.fr>
2025-03-17 18:16:29 +0100

    Some change to test

--- src/components/ex-1/Compo.jsx
+++ src/components/ex-1/Compo.jsx
@@ -1,10 +1,11 @@
+import Compo2 from "./Compo2";
 
-
-const Compo = (props) => {
-    const {name, email, age} = props;
+const Compo = ({name, email, age}) => {
+    
     return ( 
         <div>
             <h1 className="font-medium text-2xl">Hello i am a component - {name}, {email}, {age} </h1>
+            <Compo2 name={name} email={email} age={age}/>
         </div>
      );
 }
---
+++ src/components/ex-1/Compo2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Compo2 = ({name, email, age}) => {
+    console.log(email)
+  return (
+    <div>
+      <h1 className='font-bold text-2xl'>I am Compo 2</h1>
+    </div>
+  )
+}
+
+export default Compo2
